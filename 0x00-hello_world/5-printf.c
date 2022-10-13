918bd2e183cf3fbce338b3c9fecc2a647cdb5912
jahredbraps <jahredbraps>
2022-10-13 14:08:34 -0700

    printf c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#incllude <stdio.h>
+#include <stdio.h>
 /**
  * main - Entry
  * Return: Always 0
@@ -7,4 +7,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n";
 			return (0);
-}
+			}
