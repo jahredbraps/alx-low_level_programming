c4016975cd5acbd9572c63b49fe9cab30ab0d6ed
jahredbraps <jahredbraps>
2022-10-18 15:50:33 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,29 +2,18 @@
 
 /**
  * print_last_digit - draws adiagonal lines according parameter
- * @n: The number of times to print diagonal lines
- *
- * * Return: empty
+ * @n: integer params
  */
 void print_last_digit(int n)
 {
 	int x;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		for (x = 0; x < n; x++)
-		{
-			for (y = 0; y < n; y++)
-			{
-				_putchar(32);
-			}
-			_putchar(92);
-			_putchar('\n');
-
-		}
+			_putchar('\');
+		_putchar('\n');
 	}
-}	
+}
