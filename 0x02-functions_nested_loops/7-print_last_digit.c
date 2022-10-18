ae77f52f609963c6756217ace41371aaed3a579c
jahredbraps <jahredbraps>
2022-10-18 15:23:35 -0700

    print

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,30 @@
 #include "main.h"
 
 /**
- * print_last_digit -> prints the last digit
- * @n: the passed args
- * Return: the last digit
+ * print_last_digit - draws adiagonal lines according parameter
+ * @n: The number of times to print diagonal lines
+ *
+ * * Return: empty
  */
-int print_last_digit(int n)
+void print_last_digit(int n)
 {
 	int x;
 
-	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < n; y++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+
+		}
+	}
+}	
