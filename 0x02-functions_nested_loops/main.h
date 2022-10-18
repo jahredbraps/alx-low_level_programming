c14b0d2951b7c32fb77c59e8729e782708247d21
jahredbraps <jahredbraps>
2022-10-18 13:01:12 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-viod print_alphabet(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int_abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
