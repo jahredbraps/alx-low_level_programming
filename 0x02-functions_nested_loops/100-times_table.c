04d7a5937aa69873a00ffb6381cf9240f690cdc1
jahredbraps <jahredbraps>
2022-10-19 07:39:13 -0700

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - returns a combination  of number
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < 3)
+			{
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
+				_putchar(' ');
+				_putchar((j / 10) + 48);
+				_putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					Putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
