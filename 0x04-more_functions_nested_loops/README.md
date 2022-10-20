4a326261fc92d5b8c4225c5ac89ca4b7c073faf7
jahredbraps <jahredbraps>
2022-10-20 11:59:40 -0700

    readme.md

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-more functions nested loops
+0x04 more functions nested loops
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,5 +15,3 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 #endif /* MAIN_H */
-Footer
-
