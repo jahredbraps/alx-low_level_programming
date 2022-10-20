2286ac59c78f27b90708da418c7fd40f75ba2d04
jahredbraps <jahredbraps>
2022-10-20 13:15:28 -0700

    5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,18 @@ void more_numbers(void)
 {
 	int a = 0;
 	int b = 0;
-	
+
 	while (b <= 9)
 	{
 		while (a <= 14)
 		{
 			if (a > 9)
 				_putchar(a / 10 + '0');
-			_putchar(a % 10 + '0'); a++;
+			_putchar(a % 10 + '0');
+			a++;
 		}
 	}
-	_putchar('\n'); b++;
+	_putchar('\n');
+	b++;
 	a = 0;
 }
