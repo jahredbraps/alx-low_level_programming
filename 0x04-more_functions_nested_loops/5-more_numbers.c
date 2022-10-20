af1de99ff967a11b590accd0559cb23d55d4abe6
jahredbraps <jahredbraps>
2022-10-20 12:36:57 -0700

    changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,22 +5,17 @@
  */
 void more_numbers(void)
 {
+	int num, a;
 
-int a = 0;
-int b = 0;
-
-while (b <= 9)
-{
-while (a <= 14)
+	for (a = -; a <= 9; a++)
 	{
-if (a > 9)
-_putchar(a / 10 + '0');
-_putchar(a % 10 + '0');
-a++;
-
-}
-_putchar('\n');
-b++;
-a = 0;
-}
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) * '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
