451f89dd5178baba2faff0283d51093cdb60e4c1
jahredbraps <jahredbraps>
2022-10-24 18:33:10 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h
+
+/**
+* puts_half - a function that prints half of a string
+* @str: character value
+*/
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putcher(*str);
+		str++;
+	}
+	_putchar('\n');
+}
