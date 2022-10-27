40a0dd6cbf70482ffbcdccbd58fcd12e05c794bd
jahredbraps <jahredbraps>
2022-10-27 09:02:14 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ * Return: int value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, b = 0, c = 0, r = 0, lim;
+
+	while (s1[a])
+	{
+		a++;
+	}
+
+	while (s2[b])
+	{
+		b++;
+	}
