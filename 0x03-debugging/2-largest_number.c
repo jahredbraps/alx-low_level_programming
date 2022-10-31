6fd90e83812e14bced1bfc72ce73e93e1d418036
jahredbraps <jahredbraps>
2022-10-31 11:24:21 -0700

    correction

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest number
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -12,21 +12,34 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && b > c)
+		largest = a;
+	else if (a > b && b < c)
 	{
-		if (b > c)
-			largest = a;
-		else if (a  > c)
+		if (a > c)
 			largest = a;
 		else
 			largest = c;
 	}
+	else if (b > a && a > c)
+		largest = b;
+	else if (b > a && a < c)
+	{
+		if (b > c)
+			largest = b;
+		else
+			largest = c;
+	}
+	else if (a == b && a == c)
+		largest = a;
 	else
 	{
-		if (b < c)
-			largest = c;
-		else
+		if ((a == b && a > c) || (a == c && a > b) || (b == c && a > b))
+			largest = a;
+		else if ((a == c && a < b) || (b == c && a < b))
 			largest = b;
+		else
+			largest = c;
 	}
 	return (largest);
 }
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,16 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_remaining_days - picks a date
- * and prints how many is left
- * @month: month is number format
- * @day: day of mont
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+	if ((year % 400 == 0 && year % 100 == 0) || year % 4 == 0)
 	{
 		if (month >= 3 && day >= 60)
 		{
@@ -18,17 +20,17 @@ void print_remaining_days(int month, int day, int year)
 		}
 
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Invalid data: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
